81609d4c0e183664f9b7451796b4afd18e67bd9f
Dima K <dima1086@hotmail.com>
2017-10-29 17:21:28 -0400

    Updated User database. Added city, state, country, post fields.

--- setup_database.py
+++ setup_database.py
@@ -1,13 +1,9 @@
 import os
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
-
 from sqlalchemy.ext.declarative import declarative_base
-
 from sqlalchemy.orm import relationship
-
 from sqlalchemy import create_engine
-
 Base = declarative_base()
 
 
@@ -41,7 +37,11 @@ class User(Base):
     gender = Column(String(1), nullable=False)
     dob = Column(String(10), nullable=False)
     phone = Column(String(15), nullable=False)
-    address = Column(String(100), nullable=False)
+    address = Column(String(70), nullable=False)
+    city = Column(String(20), nullable=False)
+    state = Column(String(20), nullable=False)
+    country = Column(String(20), nullable=False)
+    post = Column(String(8), nullable=False)
     register_date = Column(String(10), nullable=False)
     type_id = Column(Integer, ForeignKey('types.id'))
     picture = Column(String(100))
@@ -52,16 +52,19 @@ class User(Base):
     # details_id = Column(Integer, ForeignKey("user_address.address_id"))
 
     def __init__(self, first_name, last_name, email, gender, dob,
-                 phone, address, country, register_date, type_id, picture):
+                 phone, address, city, state, country, post, register_date, type_id, picture):
+
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.gender = gender
         self.dob = dob
         self.phone = phone
-        self.address = address["street"] + ", " + address["city"] + ", " + \
-            address["state"] + ", " + str(address["postcode"]) + \
-            ", " + country
+        self.address = address
+        self.city = city
+        self.state = state
+        self.country = country
+        self.post = post
         self.register_date = register_date
         self.type_id = type_id
         self.picture = picture
@@ -78,6 +81,10 @@ class User(Base):
             'dob': self.dob,
             'phone': self.phone,
             'address': self.address,
+            'city': self.city,
+            'state': self.state,
+            'country': self.country,
+            'post': self.post,
             'register_date': self.register_date,
             'type_id': self.type_id,
             'picture': self.picture
